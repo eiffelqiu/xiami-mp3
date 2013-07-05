d66e4e2921de6cb4277e0e9847bef77643b01fcd
Eiffel Qiu <eiffelqiu@gmail.com>
2013-07-05 15:13:55 +0800

    add download album function

--- xiami.rb
+++ xiami.rb
@@ -16,6 +16,19 @@ post "/xiami" do
   end
 end
 
+post "/album" do
+  afrom = "#{params[:afrom]}".split('?').first.split('/').last
+  ato = "#{params[:ato]}".split('?').first.split('/').last
+  output = ''
+  (afrom.to_i..ato.to_i).each do |o|
+    doc = Nokogiri::XML(open("http://www.xiami.com/widget/xml-single/uid/0/sid/#{o}").read)
+    doc.search('//trackList/track/location').map do |n|
+      output << mp3url(n.text) + "<p/>"
+    end
+  end
+  output
+end
+
 def mp3url(str)
   num_loc = str.index('h')
   rows = str[0..num_loc].to_i
@@ -56,7 +69,22 @@ __END__
             url: "/xiami",
             data: $('#mp3form').serialize(),
             success: function(data){
-              $("#msg").html("歌曲 mp3 下载地址:  <font color=blue>" + data + "</font>")
+              $("#msg").html("歌曲 mp3 下载地址: <p/> <font color=blue>" + data + "</font>")
+            },
+            error: function(){
+              $("#msg").html("No MP3")
+            }
+          });
+        });
+
+        $("form#albumform").submit(function(e){
+          e.preventDefault();
+          $.ajax({
+            type: "POST",
+            url: "/album",
+            data: $('#albumform').serialize(),
+            success: function(data){
+              $("#msg").html("专辑 mp3 下载地址: <p/> <font color=blue>" + data + "</font>")
             },
             error: function(){
               $("#msg").html("No MP3")
@@ -68,14 +96,28 @@ __END__
     %center
       %div(class="container")
         %div(class="hero-unit")
-          %h4 虾米 mp3 获取器
           = yield
 
 @@index
+%h4 虾米单曲 mp3 下载
 %form#mp3form(action="/xiami" method="POST")
   %div 输入虾米歌曲地址(例如: http://www.xiami.com/song/369173?spm=0.0.0.0.IAjRbk):
   %p
   %input#word(type="text" name="xid" class="span5 input-large")
   %p
   %input(type="submit" value="生成 MP3 下载地址" class="btn btn-primary btn-large")
+%hr
+%p
+%h4 虾米专辑 mp3 下载
+%form#albumform(action="/album" method="POST")
+  %p
+  %div 输入专辑第一首歌曲地址(例如: http://www.xiami.com/song/369173?spm=0.0.0.0.IAjRbk):
+  %p
+  %input#word(type="text" name="afrom" class="span5 input-large")
+  %p
+  %div 输入专辑最后一首歌曲地址(例如: http://www.xiami.com/song/369173?spm=0.0.0.0.IAjRbk):
+  %p
+  %input#word(type="text" name="ato" class="span5 input-large")
+  %p
+  %input(type="submit" value="生成专辑 MP3 下载地址" class="btn btn-primary btn-large")
 #msg
